b55d3398323aececd55ef8e1753b4a1b63a2a14d
AngelGallegos <7vonengel@gmail.com>
2016-12-04 22:47:48 -0600

    Corregido Product no mayor a 100 en vez de no mayor a 99

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
-  validates_numericality_of :price, :less_than_or_equal_to => 99, :message => "El precio debe ser menor a 100"
+  validates_numericality_of :price, :less_than_or_equal_to => 100, :message => "El precio no puede ser mayor a 100"
   belongs_to :brand
 end
