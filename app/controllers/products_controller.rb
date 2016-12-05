50db6f49c3b233b372ca03c5057881b87e7954e1
AngelGallegos <7vonengel@gmail.com>
2016-12-04 19:11:28 -0600

    Modificado index y mensajes de actualizacion para  Marcas y Productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to products_url, notice: 'El producto fue actualizado exitósamente' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -62,7 +62,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url, notice: 'El producto fue eliminado exitósamente' }
       format.json { head :no_content }
     end
   end
